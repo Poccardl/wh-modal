89421fc154e3e932123c413c0a9866d4d0411757
Poccardl <loic.poccard@gmail.com>
2021-05-24 02:00:30 +0200

    init WhModal

---
+++ src/views/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WhModal from '../wh-modal/WhModal.jsx'
+
+function App() {
+  return (
+    <>
+    <WhModal />
+    </>
+  )
+}
+
+export default App
---
+++ src/wh-modal/WhModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './modal.css'
+
+function WhModal(props) {
+    return (
+        <>
+        <div>
+            WhModal
+        </div>
+        </>
+    )
+}
+
+export default WhModal
